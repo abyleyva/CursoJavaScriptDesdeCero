b14b2f899ec603fa66f0bc31689fdfeab99b0b5d
Aby Leyva <abyleyva75@gmail.com>
2023-11-28 14:42:19 -0600

    First Commit fundamentals JavaScript

---
+++ 05-clase-variables.js
@@ -0,0 +1,23 @@
+// se utiliza la palabra reservada let para declaracion de variables
+let cajaDeAndy='Woody';
+
+console.log(cajaDeAndy);
+
+// LO NO PERMITIDO EN NOMBRAMIENTOS DE VARIABLES
+// Nombres d Variables Genericos, debe de ser lo mas descriptivo posible
+// No importa que sea muy largo
+let c='Woody';
+let cda='Woody';
+let pcAndy='Woody';
+
+
+// LO SI PERMITIDO EN NOMBRAMIENTOS DE VARIABLES
+//Nombre Descriptivo, Abreviaciones conocidas a nivel general, variables consecutivas una debajo de la otra al inicio de todo el codigo,
+// el uso de punto y coma es opcional pero recomendable es a criterio personal, en JavaScript el programa funciona igual con o sin punto y coma
+let primeraCajaDeAndy='Woody';
+let urlDelUsuario='https://www.googloe.com';
+let idDelUsuario='9999';
+
+
+// se utiliza la palabra reservada const para declaracionoes de variable que no cambian o que son constante
+const pi=3.1416;
